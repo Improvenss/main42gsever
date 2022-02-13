2ef91c943cfd91276c668bc6e2e3014d61916a73
Gorkem Sever <gsever@k2m07s01.42kocaeli.com.tr>
2022-02-13 17:50:04 +0300

    ready_for_evo

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gsever <gsever@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 14:35:34 by gsever            #+#    #+#             */
-/*   Updated: 2022/02/13 13:52:04 by gsever           ###   ########.fr       */
+/*   Updated: 2022/02/13 17:44:34 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,3 +33,51 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (begin);
 }
+/*
+void	*ft_map(void *ct)
+{
+	int i = -1;
+	char	*pouet;
+
+	pouet = strdup((char *)ct);
+	while (pouet[++i]!='\0')
+		if (pouet[i] == 's')
+			pouet[i] = 'o';
+	return ((void *)pouet);
+}
+void ft_del(void *content)
+{	
+	//bzero(content, sizeof(content));
+	memset(content, 0, strlen(content));
+}
+
+int	main()
+{	
+	char l[] = "start";
+	char s0[] = "student0";
+	char s1[] = "student1";
+	char s2[] = "student2";
+
+	t_list *lst = ft_lstnew(l);
+	t_list *student0 = ft_lstnew(s0);
+	t_list *student1 = ft_lstnew(s1);
+	t_list *student2 = ft_lstnew(s2);
+	
+	ft_lstadd_back(&lst, student0);
+	ft_lstadd_back(&lst, student1);
+	ft_lstadd_back(&lst, student2);
+
+	t_list *newlist = ft_lstmap(lst, ft_map, ft_del);
+	while(lst)
+	{
+		puts((char *)lst->content);
+		lst = lst->next;
+	}
+
+	while(newlist)
+	{
+		puts((char *)newlist->content);
+		newlist = newlist->next;
+	}
+}
+*/
