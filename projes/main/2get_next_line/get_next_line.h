2a28ad28a02c64581ce2e4280230806162e25461
Gorkem Sever <gsever@k2m07s01.42kocaeli.com.tr>
2022-02-18 19:57:33 +0300

    re_making

--- projes/main/2get_next_line/get_next_line.h
+++ projes/main/2get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gsever <gsever@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 12:46:10 by gsever            #+#    #+#             */
-/*   Updated: 2022/02/18 16:54:52 by gsever           ###   ########.fr       */
+/*   Updated: 2022/02/18 19:56:31 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,6 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*ft_read_to_left_str(int fd, char *left_str);
-char	*ft_get_line(char *left_str);
-char	*ft_new_left_str(char *left_str);
+
 //UTILS
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen(const char *s);
 #endif
