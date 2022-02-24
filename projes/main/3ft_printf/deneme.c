edb1d5f8eaac975c849dc5b8eb7093be20c0c9f5
Gorkem Sever <gsever@k2m07s01.42kocaeli.com.tr>
2022-02-24 15:14:47 +0300

    ft_printf_started

---
+++ projes/main/3ft_printf/deneme.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   deneme.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 13:25:34 by gsever            #+#    #+#             */
+/*   Updated: 2022/02/24 13:55:10 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+/*
+int	main()
+{
+	int	sample;
+	
+	for (sample = 0; sample <= 100; sample++)
+	{
+		printf("Decimal value: %d, Character value: %c\n", sample, sample);
+	}
+}
+*/
+/*
+int	main()
+{
+	char	weekday[] = "Thursday";
+	char	month[] = "February";
+	int		day = 24;
+	int		hour = 1;
+	int		minute = 28;
+
+	fprintf(stdout, "%s, %s %d, %.2d:%.2d\n", weekday, month, day, hour, minute);
+}
+*/
