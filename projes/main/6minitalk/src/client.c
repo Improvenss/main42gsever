34d7502c76b0beb63a7d4391cfd18444b840c941
Görkem Sever <yuandre@Gorkems-MacBook-Pro.local>
2022-03-20 05:13:55 +0300

    minitalk_started

--- projes/main/6minitalk/src/client.c
+++ projes/main/6minitalk/src/client.c
@@ -6,17 +6,9 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 00:47:03 by gsever            #+#    #+#             */
-/*   Updated: 2022/03/20 04:35:27 by gsever           ###   ########.fr       */
+/*   Updated: 2022/03/20 05:13:28 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
-{
-	struct	sigaction	s_sigaction;
-	ft_putstr_fd("Server PID ", 1);
-	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd("\n", 1);
-	
-}
--- projes/main/6minitalk/src/server.c
+++ projes/main/6minitalk/src/server.c
@@ -6,9 +6,17 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 00:47:18 by gsever            #+#    #+#             */
-/*   Updated: 2022/03/20 04:19:36 by gsever           ###   ########.fr       */
+/*   Updated: 2022/03/20 05:13:26 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	main(void)
+{
+	struct	sigaction	s_sigaction;
+	ft_putstr_fd("Server PID ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd("\n", 1);
+	
+}
