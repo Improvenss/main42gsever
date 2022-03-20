f114118f35425004865917b04bb263e9cc6eaed0
Görkem Sever <yuandre@Gorkems-MacBook-Pro.local>
2022-03-21 00:56:09 +0300

    minitalk_bonus_finished

--- projes/main/6minitalk/src/client.c
+++ projes/main/6minitalk/src/client.c
@@ -5,10 +5,65 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/20 00:47:03 by gsever            #+#    #+#             */
-/*   Updated: 2022/03/20 05:13:28 by gsever           ###   ########.fr       */
+/*   Created: 2022/03/21 00:12:50 by gsever            #+#    #+#             */
+/*   Updated: 2022/03/21 00:13:02 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static void	action(int sig)
+{
+	static int	received = 0;
+
+	if (sig == SIGUSR1)
+		++received;
+	else
+	{
+		ft_putnbr_fd(received, 1);
+		ft_putchar_fd('\n', 1);
+		exit(0);
+	}
+}
+
+static void	mt_kill(int pid, char *str)
+{
+	int		i;
+	char	c;
+
+	while (*str)
+	{
+		i = 8;
+		c = *str++;
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3 || !ft_strlen(argv[2]))
+		return (1);
+	ft_putstr_fd("Sent    : ", 1);
+	ft_putnbr_fd(ft_strlen(argv[2]), 1);
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd("Received: ", 1);
+	signal(SIGUSR1, action);
+	signal(SIGUSR2, action);
+	mt_kill(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
+	return (0);
+}
--- projes/main/6minitalk/src/server.c
+++ projes/main/6minitalk/src/server.c
@@ -5,18 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/20 00:47:18 by gsever            #+#    #+#             */
-/*   Updated: 2022/03/20 05:13:26 by gsever           ###   ########.fr       */
+/*   Created: 2022/03/21 00:12:14 by gsever            #+#    #+#             */
+/*   Updated: 2022/03/21 00:13:01 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static void	action(int sig, siginfo_t *info, void *context)
+{
+	static int				i = 0;
+	static pid_t			client_pid = 0;
+	static unsigned char	c = 0;
+
+	(void)context;
+	if (!client_pid)
+		client_pid = info->si_pid;
+	c |= (sig == SIGUSR2);
+	if (++i == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			kill(client_pid, SIGUSR2);
+			client_pid = 0;
+			return ;
+		}
+		ft_putchar_fd(c, 1);
+		c = 0;
+		kill(client_pid, SIGUSR1);
+	}
+	else
+		c <<= 1;
+}
+
 int	main(void)
 {
-	struct	sigaction	s_sigaction;
-	ft_putstr_fd("Server PID ", 1);
+	struct sigaction	s_sigaction;
+
+	ft_putstr_fd("Server PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd("\n", 1);
-	
+	ft_putchar_fd('\n', 1);
+	s_sigaction.sa_sigaction = action;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
+	while (1)
+		pause();
+	return (0);
 }
