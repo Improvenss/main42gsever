900705a572f9049ebb4b721c45eeb258614b4b91
Görkem Sever <yuandre@Gorkems-MacBook-Pro.local>
2022-03-20 04:36:04 +0300

    minitalk_started

--- projes/main/6minitalk/src/client.c
+++ projes/main/6minitalk/src/client.c
@@ -6,9 +6,17 @@
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 00:47:03 by gsever            #+#    #+#             */
-/*   Updated: 2022/03/20 01:58:10 by gsever           ###   ########.fr       */
+/*   Updated: 2022/03/20 04:35:27 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	main(void)
+{
+	struct	sigaction	s_sigaction;
+	ft_putstr_fd("Server PID ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd("\n", 1);
+	
+}
