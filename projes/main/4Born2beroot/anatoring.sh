11982e6b88ccb193d3168145af4efca8ee2a97bd
Improvenss <gorkem_se_98@hotmail.com>
2022-03-29 13:14:11 +0300

    montoring.sh_configured

--- projes/main/4Born2beroot/anatoring.sh
+++ projes/main/4Born2beroot/anatoring.sh
@@ -4,12 +4,12 @@ pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
 vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
 fram=$(free -m | awk '$1 == "Mem:" {print $2}')
 uram=$(free -m | awk '$1 == "Mem:" {print $3}')
-pram=$(free | awk '$1 == "Mem:" {printf("%.2f", $3/$2*100)}')
-fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{totaldisk += $2} END {print totaldisk}')
-udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{useddisk += $3} END {print useddisk}')
-pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{useddisk += $3} {totaldisk += $2} END {printf("%d"), useddisk/totaldisk*100}')
+pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
+udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
+pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
 cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-lb=$(uptime -s | cut -c -16)
+lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
 lvmt=$(lsblk | grep "lvm" | wc -l)
 lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
 #You need to install net tools for the next step [$ sudo apt install net-tools]
@@ -30,4 +30,3 @@ wall "	#Architecture: $arc
 	#User log: $ulog
 	#Network: IP $ip ($mac)
 	#Sudo: $cmds cmd" # broadcast our system information on all terminals
-
