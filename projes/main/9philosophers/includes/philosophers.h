eb7e9d7dde504537da2ff36207237816e17b2622
Gorkem Sever <gsever@k2m14s06.42kocaeli.com.tr>
2022-07-31 18:36:39 +0300

    includes added

---
+++ projes/main/9philosophers/includes/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/31 18:33:22 by gsever            #+#    #+#             */
+/*   Updated: 2022/07/31 18:36:21 by gsever           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/** @file philosophers.h
+ * @brief Libraries for 'philosophers' project.
+ * @author Görkem SEVER (gsever)
+ * @bug Not know bugs.
+ */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <
